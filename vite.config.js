3a51d5ab546be595ffa4cd877e981b7f290bbadd
xbru1 <xbrumley.edu@gmail.com>
2024-11-24 08:00:57 -0800

    Last Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping",
     plugins: [react()],
     mode: 'development', // Optional, dev mode is default
     build: {
